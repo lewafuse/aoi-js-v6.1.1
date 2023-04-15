724610cc6d6176fe5c9633234b752d70e55eaac3
Lewafuse <87063319+lewafuse@users.noreply.github.com>
2023-04-15 15:50:49 +0300

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res){
-  res.write("Actived");//İsteğe bağlı
+  res.write("Actived by ROCA COMMUNITY");//İsteğe bağlı
   res.end();
 }).listen(1951);//By Lewafuse
